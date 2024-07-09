11a231fc5757c9fc38991ff20a284b3bf3b48d8d
Amar Husain <amar.husain156@gmail.com>
2024-07-09 16:40:51 +0530

    modified fe and be

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,16 @@
 import * as bodyparser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
-
 
 const app: express.Application = express();
 
 app.use(bodyparser.json());
 app.use(cors());
-
+dotenv.config();
 
 app.get('/health', (req: Request, res: Response) => {
-    const message = `Server is running at http://localhost:${process.env.port}`;
+    const message = `Server is running at http://localhost:${process.env.SERVER_PORT}`;
     const timestamp = new Date().toLocaleString();
     res.status(200).send({ message, timestamp });
 });
@@ -18,8 +18,8 @@ app.get('/health', (req: Request, res: Response) => {
 
 (async () => {
     try {
-        app.listen(process.env.port, () => {
-            console.log(`⚡️[server]: Server is running at http://localhost:${process.env.port}`);
+        app.listen(process.env.SERVER_PORT, () => {
+            console.log(`⚡️[server]: Server is running at http://localhost:${process.env.SERVER_PORT}`);
         });
     } catch (err) {
         throw new Error(`[ERROR]: Couldn't start server`)
